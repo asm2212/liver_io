971422821db5e8f6c1a5b2130b4f9021c994f839
asm2212 <asmareadmasu0@gmail.com>
2025-01-08 11:02:50 +0300

    Refactor welcome screen layout and add constants for color management

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liver_io/utils/constants.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -10,9 +11,105 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text('Welcome Screen'),
+      backgroundColor: Constants.backgroundColor,
+      body: SingleChildScrollView(
+        child: Container(
+          color: Constants.backgroundColor, 
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: media.width * 1.7, 
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, 
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 2,
+                  ),
+                  itemCount: 6, 
+                  itemBuilder: (context, index) {
+                    return Transform.rotate(
+                      angle: -0.1,
+                      child: Image.asset(
+                        'assets/images/wel${index + 1}.png', 
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              // SizedBox(height: media.height * 0.02),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0,), 
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Liver.',
+                            style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: Constants.white,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'io',
+                            style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: Constants.red,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    
+                    Text(
+                      "Stream, watch other games streams and play games in one app",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Constants.primaryText,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    SizedBox(height: media.height * 0.02),
+                    InkWell(
+                      onTap: () {
+                        
+                      },
+                      child: Container(
+                        width: media.width * 0.8,
+                        height: media.height * 0.06,
+                        decoration: BoxDecoration(
+                          color: Constants.red,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Start",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Constants.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static Color get red => const Color(0xffFF3939);
+  static Color get primary => const Color(0xff4CD080);
+  static Color get backgroundColor => const Color(0xff160418);
+  static Color get white => const Color(0xffffffff);
+  static Color get black => const Color(0xff000000);
+  static Color get primaryText => const Color(0xffB3A5DA);
+
+}
