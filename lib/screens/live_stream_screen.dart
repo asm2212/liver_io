b90e7758907ec34a39c542a38b069c2b036df3b2
asm2212 <asmareadmasu0@gmail.com>
2025-01-09 10:52:43 +0300

    Add live stream details and chat input section to live stream screen

--- lib/screens/live_stream_screen.dart
+++ lib/screens/live_stream_screen.dart
@@ -29,7 +29,7 @@ class _LiveStreamScreenState extends State<LiveStreamScreen> {
               AppBar(
                 automaticallyImplyLeading: false,
                 backgroundColor: Constants.backgroundColor,
-                elevation: 0, 
+                elevation: 0,
                 title: Row(
                   children: [
                     Row(
@@ -90,15 +90,99 @@ class _LiveStreamScreenState extends State<LiveStreamScreen> {
                   ],
                 ),
               ),
-               
-            ],
-            
-          ),
+              SizedBox(
+                height: media.height * 0.05,
+              ),
+              Row(
+                children: [
+                  Image.asset(
+                    "assets/images/pu1.png",
+                    width: 40,
+                    height: 40,
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Carry is Live",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Constants.white,
+                        ),
+                      ),
+                      Text(
+                        "234k views",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Constants.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: media.height * 0.02,
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: media.height * 0.06,
+                    width: media.width * 0.6,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Constants.white,
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(26),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Add chat...",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Constants.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Spacer(),
+                          Container(
+                              height: 36,
+                              width: 36,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Constants.red,
+                              ),
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                color: Constants.white,
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: media.width * 0.05),
+                  Container(
+                    height: 45,
+                    width: 45,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                         color: Constants.red,
+                    ),
+                    
+                    )
+                ],
+              ),
 
+            ],
+          ),
         ],
       ),
-
-
     );
   }
 }
